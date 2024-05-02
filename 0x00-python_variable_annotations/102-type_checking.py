169c31b920afcc7213775b1cab1fc5638e169a56
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-02 11:30:01 +0100

    mypy type verification ;

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,12 +1,17 @@
 #!/usr/bin/env python3
-"""Type-annotated function safely_get_value"""
-from typing import Union, Any, Mapping
+"""Type-annotated function zoom_array that takes a tuple lst as argument"""
+from typing import Tuple
 
 
-def safely_get_value(dct: Mapping, key: Any,
-                        default: Union[Any, None] = None) -> Union[Any, None]:
-        """Return the value with the specified key if exists, otherwise None"""
-        if key in dct:
-            return dct[key]
-        else:
-            return default
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """Return a tuple of tuples"""
+    zoomed_in: Tuple = tuple(item for item in lst
+                             for i in range(factor))
+    return zoomed_in
+
+
+array = [12, 72, 91]
+
+zoom_2x = zoom_array(tuple(array))
+
+zoom_3x = zoom_array(tuple(array), 3.0)
