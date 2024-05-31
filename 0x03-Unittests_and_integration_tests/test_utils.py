b42f223bbde43a949220e90243bb369e79c9a48c
Alhadji Oumate <alhadjioum0@gmail.com>
2024-05-31 12:18:02 +0100

    test the utils python file

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,17 +2,12 @@
 """test for utils.access_nested_map"""
 from utils import access_nested_map
 import unittest
-from typing import (
-    Mapping,
-    Sequence,
-    Any,
-    Dict,
-    Callable,
-)
 
 
 class TestAccessNestedMap(unittest.TestCase):
+    """Test access_nested_map function."""
     def test_access_nested_map(self):
+        """Test access_nested_map function."""
         nested_map = {"a": 1}
         self.assertEqual(access_nested_map(nested_map, ("a",)), 1)
         nested_map = {"a": {"b": 2}}
