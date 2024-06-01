d672c4acbe25d857fb1730be7e13cab8d2f246b0
Alhadji Oumate <alhadjioum0@gmail.com>
2024-06-01 02:13:51 +0100

    test the client module

---
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""test for client module"""
+from client import GithubOrgClient
+from parameterized import parameterized
+from unittest.mock import patch, Mock, PropertyMock
+from unittest import TestCase, main
+import client
+
+
+class TestGithubOrgClient(TestCase):
+    """class to test the GithubOrgClient class"""
+    @parameterized.expand([
+        ("google"),
+        ("abc"),
+    ])
+    @patch('client.get_json')
+    def test_org(self, org_name, mock_get_json):
+        """test the org method"""
+        test_class = GithubOrgClient(org_name)
+        test_class.org()
+        mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+
+if __name__ == "__main__":
+    main()
